8322c98e4aa77eca664d178dfd3e76e5dc16f821
Justin Stone <ntropish@gmail.com>
2025-06-26 19:32:36 -0500

    Configure client and hlc packages

---
+++ packages/client/src/index.ts
@@ -0,0 +1,9 @@
+// The high-level ZG client runtime will be implemented here.
+
+export function createClient(options: any) {
+  // TODO: Implement client creation
+  console.log("Client created with options:", options);
+  return {
+    // This will be populated with schema-aware collections
+  };
+}
