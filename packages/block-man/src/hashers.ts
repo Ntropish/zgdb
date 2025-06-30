676d04323eb5738a8b08025d105975e5bf28ad4e
Justin Stone <ntropish@gmail.com>
2025-06-29 20:49:39 -0500

    Remove block-man package and associated files, including package.json, PRD, TypeScript configuration, and source files. This cleanup prepares for a new implementation of the block store functionality.

--- packages/block-man/src/hashers.ts
+++
@@ -1,7 +0,0 @@
-import { webcrypto } from "node:crypto";
-import type { Hasher } from "./types.js";
-
-export const sha256: Hasher = async (data: Uint8Array): Promise<Uint8Array> => {
-  const hash = await webcrypto.subtle.digest("SHA-256", data);
-  return new Uint8Array(hash);
-};
--- packages/block-man/src/index.ts
+++
@@ -1,3 +0,0 @@
-export type { BlockStore, Address, Hasher } from "./types.js";
-export { MemoryBlockStore } from "./memory-block-store.js";
-export { sha256 } from "./hashers.js";
